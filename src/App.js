49504107dc66106b47112c82cfde8afcbea82317
Darshkumar Jasani <jasanidarsh3@gmail.com>
2023-10-24 21:56:42 -0700

    File Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar  from './Nav/Navbar'
+import Skill from './Skills/Skill';
 import './App.css';
 function App() {
   return (
     <>
       <Navbar/>
+      <Skill/>
     </>
   )
 }
---
+++ src/Skills/Skill.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Skill.css'
+function Skill() {
+  return (
+   <>
+    <div className='skill'>
+        <div class="gallery">
+            <img src="https://www.cnet.com/a/img/resize/6f59de6f421aca65461420ada94716b37c756c87/hub/2011/01/18/8d118cb0-fdc0-11e2-8c7c-d4ae52e62bcc/HTML5-wow.png?auto=webp&fit=crop&height=675&width=1200" alt="a house on a mountain" />
+            <img src="https://2.bp.blogspot.com/-aVgpvwLTuzE/UysB6qMb1mI/AAAAAAAAAgE/aLXSX6mHIv8/s1600/%D8%A5%D8%B6%D8%A7%D9%81%D8%A9+%D8%AA%D8%A3%D8%AB%D9%8A%D8%B1%D8%A7%D8%AA+css+%D8%B9%D9%84%D9%89+%D8%A7%D9%84%D8%B5%D9%88%D8%B1.png" alt="sime pink flowers"/>
+            <img src="https://miro.medium.com/v2/resize:fit:800/1*M_GDn6RXZSZC66kvrMHh5Q.png" alt="big rocks with some trees"/>
+            <img src="https://picsum.photos/id/1039/300/300" alt="a waterfall, a lot of tree and a great view from the sky"/>
+            <img src="https://picsum.photos/id/110/300/300" alt="a cool landscape"/>
+            <img src="https://picsum.photos/id/1047/300/300" alt="inside a town between two big buildings"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+            <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+
+
+        </div>
+    </div>
+   </>
+  )
+}
+
+export default Skill
